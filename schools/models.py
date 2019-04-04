af19ce777d96d6413f20d590c76b132deec4e3f0
Arunkrishnan <hereisarunkrishnan@gmail.com>
2019-04-04 12:22:23 +0530

    Make school code mandatory

--- schools/models.py
+++ schools/models.py
@@ -9,7 +9,7 @@ from utils.model_templates import LogicalDeleteModel
 class School(LogicalDeleteModel):
     id = UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255)
-    code = models.CharField(max_length=7, null=True, blank=True)
+    code = models.CharField(max_length=7, unique=True)
 
     def __str__(self):
         return f"{self.code}-{self.name}"
